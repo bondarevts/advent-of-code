0b1b0dfb3b85caf2adec96e4c2a5eb61cbe13819
Timofei Bondarev <bondarevts@gmail.com>
2023-12-02 17:08:59 -0500

    Setup a TS project for 2023

---
+++ 2023/dispatcher.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs'
+
+async function runSolution() {
+  const day = process.argv[3]
+  const level = (process.argv[4] ?? '1') as '1' | '2'
+  const dayFolder = `day-${day}`
+
+  console.log(`Running solution for day ${day}, level ${level}`)
+
+  const solution = await import(`./${dayFolder}/solution.ts`)
+
+  const datasetFolder = `${process.cwd()}/${dayFolder}`
+  let dataset: string
+  switch (process.argv[2]) {
+    case "solve":
+      dataset = `${datasetFolder}/dataset.txt`
+    
+      break;
+    case "test": 
+      dataset = `${datasetFolder}/test${level}.txt`
+      break;
+    default:
+      throw new Error("Wrong mode")
+  }
+
+  console.log(`Running on the dataset: ${dataset}`)
+  const data = fs.readFileSync(dataset, 'utf8')
+  solution[`solve${level}`](data)
+}
+
+runSolution()
