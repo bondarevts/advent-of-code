245998ba421912f4a8e99858d5d1453414dafab9
Timofei Bondarev <bondarevts@gmail.com>
2023-12-04 10:24:06 -0500

    Solution for 2023, day 3, part 2

--- 2023/day-3/solution.ts
+++ 2023/day-3/solution.ts
@@ -10,11 +10,32 @@ type Position = {
 
 export function solve1(data: string): void {
   const lines = data.trimEnd().split('\n')
-  const potentialPartNumbers = findPotentialPartNumbers(lines)
-  const result = potentialPartNumbers
-    .filter((p) => isLegalPartNumber(p, lines))
+  const result = findAllPartNumbers(lines)
     .reduce((acc, partNumber) => acc + Number(partNumber.value), 0)
   console.log(result)
+}
+
+export function solve2(data: string): void {
+  const lines = data.trimEnd().split("\n")
+  const potentialGears = findPotentialGears(lines)
+
+  const partNumbers = findAllPartNumbers(lines)
+  const result = 
+    potentialGears
+    .map(position => getBorderNumberPositions(position, lines))
+    .map(gearNumberPositions => {
+      const gearPartNumbers = gearNumberPositions.map(position => getPartByPosition(position, partNumbers))
+      return [...new Set(gearPartNumbers)]
+    })
+    .filter(partNumbers => partNumbers.length == 2)
+    .map(([part1, part2]) => Number(part1.value) * Number(part2.value))
+    .reduce((acc, value) => acc + value, 0)
+  console.log(result)
+}
+
+function findAllPartNumbers(lines: string[]): PartNumber[] {
+  const potentialPartNumbers = findPotentialPartNumbers(lines)
+  return potentialPartNumbers.filter((p) => isLegalPartNumber(p, lines))
 }
 
 function findPotentialPartNumbers(lines: string[]): PartNumber[] {
@@ -65,4 +86,37 @@ function isLegalPartNumber(partNumber: PartNumber, lines: string[]): boolean {
     partNumber.value.length,
     lines,
   ).some((position) => lines[position.y][position.x] != '.')
+}
+
+function findPotentialGears(lines: string[]): Position[] {
+  return lines.flatMap((line, index) => parseGearsFromLine(line, index))
+}
+
+function parseGearsFromLine(line: string, index: number): Position[] {
+  const positions: Position[] = []
+  for (const match of line.matchAll(/\*/g)) {
+    positions.push({y: index, x: match.index!})
+  }
+  return positions
+}
+
+function getBorderNumberPositions(position: Position, lines: string[]): Position[] {
+  const numbers: number[] = []
+  return getValidBorderPositions(position, 1, lines)
+  .map(position => {
+    return {
+    position: position,
+    value: lines[position.y][position.x]
+  }})
+  .filter(({value}) => /\d/.test(value))
+  .map(({position}) => position)
+}
+
+function getPartByPosition(position: Position, partNumbers: PartNumber[]): PartNumber {
+  for (const partNumber of partNumbers) {
+    if (position.y == partNumber.position.y && position.x >= partNumber.position.x && position.x < partNumber.position.x + partNumber.value.length) {
+      return partNumber
+    }
+  }
+  throw new Error(`Can't find a part number for ${position}`)
 }
